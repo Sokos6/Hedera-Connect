dbb17cdcbb5e2821a9df9e5e5ac3127561524843
Sokos6 <will.sokolowski@gmail.com>
2021-01-20 21:52:50 -0600

    remove comments

--- index.js
+++ index.js
@@ -9,40 +9,31 @@ const {
 require('dotenv').config();
 
 async function main() {
-    //Grab your Hedera testnet account ID and private key from your .env file
     const myAccountId = process.env.MY_ACCOUNT_ID;
     const myPrivateKey = process.env.MY_PRIVATE_KEY;
 
-    // If we weren't able to grab it, we should throw a new error
     if (myAccountId == null || myPrivateKey == null) {
         throw new Error(
             'Environment variables myAccountId and myPrivateKey must be present'
         );
     }
-
-    // Create our connection to the Hedera network
-    // The Hedera JS SDK makes this reallyyy easy!
     const client = Client.forTestnet();
 
     client.setOperator(myAccountId, myPrivateKey);
 
-    //Create new keys
     const newAccountPrivateKey = await PrivateKey.generate();
     const newAccountPublicKey = newAccountPrivateKey.publicKey;
 
-    //Create a new account with 1,000 tinybar starting balance
     const newAccountTransactionResponse = await new AccountCreateTransaction()
         .setKey(newAccountPublicKey)
         .setInitialBalance(Hbar.fromTinybars(1000))
         .execute(client);
 
-    // Get the new account ID
     const getReceipt = await newAccountTransactionResponse.getReceipt(client);
     const newAccountId = getReceipt.accountId;
 
     console.log('The new account ID is: ' + newAccountId);
 
-    //Verify the account balance
     const accountBalance = await new AccountBalanceQuery()
         .setAccountId(newAccountId)
         .execute(client);
@@ -58,7 +49,6 @@ async function main() {
         .addHbarTransfer(newAccountId, Hbar.fromTinybars(1000))
         .execute(client);
 
-    //Verify the transaction reached consensus
     const transactionReceipt = await transferTransactionResponse.getReceipt(
         client
     );
@@ -67,14 +57,12 @@ async function main() {
             transactionReceipt.status.toString()
     );
 
-    //Request the cost of the query
     const getBalanceCost = await new AccountBalanceQuery()
         .setAccountId(newAccountId)
         .getCost(client);
 
     console.log('The cost of query is: ' + getBalanceCost);
 
-    //Check the new account's balance
     const getNewBalance = await new AccountBalanceQuery()
         .setAccountId(newAccountId)
         .execute(client);
